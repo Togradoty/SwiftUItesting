750df019c26eba4c54aeb546131e9d98bd883a4d
Togradoty <tlsmithmn@gmail.com>
2023-04-09 18:19:08 -0500

    Initial Commit

---
+++ SwiftUItesting/SwiftUItestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUItestingApp.swift
+//  SwiftUItesting
+//
+//  Created by Tyler Smith on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUItestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
